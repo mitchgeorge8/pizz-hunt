aa21183621d4b7222c4303220be6b4e6697cf8fa
Mitchell George <mitchgeorge8@gmail.com>
2022-06-11 16:29:11 -0400

    18.4.7 Upload the pizza data

--- public/assets/js/idb.js
+++ public/assets/js/idb.js
@@ -11,7 +11,7 @@ request.onsuccess = function (event) {
   db = event.target.result;
 
   if (navigator.onLine) {
-    // uploadPizza();
+    uploadPizza();
   }
 };
 
@@ -25,3 +25,38 @@ function saveRecord(record) {
 
   pizzaObjectStore.add(record);
 }
+
+function uploadPizza() {
+  const transaction = db.transaction(["new_pizza"], "readwrite");
+  const pizzaObjectStore = transaction.objectStore("new_pizza");
+  const getAll = pizzaObjectStore.getAll();
+
+  getAll.onsuccess = function () {
+    if (getAll.result.length > 0) {
+      fetch("/api/pizzas", {
+        method: "POST",
+        body: JSON.stringify(getAll.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((serverResponse) => {
+          if (serverResponse.message) {
+            throw new Error(serverResponse);
+          }
+          const transaction = db.transaction(["new_pizza"], "readwrite");
+          const pizzaObjectStore = transaction.objectStore("new_pizza");
+
+          pizzaObjectStore.clear();
+          alert("All saved pizza has been submitted!");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+}
+
+window.addEventListener("online", uploadPizza);
